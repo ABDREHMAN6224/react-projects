dc2e30a7ee77c34ab582c5985ea3167ade057fad
raja <memonabdulrehman250@gmail.com>
2023-06-26 00:21:43 +0500

    hero corrected

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,19 +2,25 @@ import React from 'react'
 import heroImg from "./assets/hero.svg"
 const Hero = () => {
   return (
-    <section className='hero'>
-        <div className="hero-center">
-            <div className="hero-title">
-                <h1>Contentful CMS</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda natus ea distinctio vitae exercitationem unde voluptatibus, magnam, aliquam odit quibusdam adipisci porro in vero corporis, sapiente maxime nemo. Consequuntur, unde?</p>
-            </div>
-            <div className="img-container">
-                <img src={heroImg} alt="" className='img'/>
-            </div>
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>React Projects</h1>
+          <p>
+            I have developed a diverse React project portfolio showcasing my
+            skills and creativity. From responsive web applications to
+            interactive UI components, my projects demonstrate my ability to
+            build robust and visually appealing user interfaces. With a focus on
+            clean code and efficient performance, my portfolio reflects my
+            dedication to delivering high-quality React projects.
+          </p>
         </div>
-
+        <div className="img-container">
+          <img src={heroImg} alt="" className="img" />
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Hero
